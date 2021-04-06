d8c550e43c865c90c0aad16a3b539f70cc55abc8
Phil Crossland <phil@thirdwheelstudios.com>
2021-04-06 21:08:56 +0100

    End of L3 - Multi v-model bindings

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
-
+    <SalutationName
+      v-model:salutation="form.salutation"
+      v-model:name="form.name"
+    />
     <pre>{{ form }}</pre>
   </div>
 </template>
 
 <script>
+import { reactive } from 'vue'
+import SalutationName from '@/components/SalutationName'
+
 export default {
   name: 'App',
   components: {
-
+    SalutationName
   },
   setup () {
+    const form = reactive({
+      salutation: '',
+      name: ''
+    })
 
+    return { form }
   }
 }
 </script>
--- src/components/SalutationName.vue
+++ src/components/SalutationName.vue
@@ -1,24 +1,43 @@
 <template>
   <div>
+    <select @change="$emit('update:salutation', $event.target.value)">
+      <option value=""></option>
+      <option
+        v-for="(item, index) in salutations"
+        :key="index"
+        :value="item"
+        :selected="salutation === item"
+      >
+        {{ item }}
+      </option>
+    </select>
 
+    <input
+    @input="$emit('update:name', $event.target.value)"
+      :input="name"
+      type="text"
+    />
   </div>
 </template>
 
 <script>
-const salutations = [
-  'Ms.',
-  'Mrs.',
-  'Miss',
-  'Mx.',
-  'Dr.'
-]
+const salutations = ['Ms.', 'Mrs.', 'Miss', 'Mx.', 'Dr.']
 
 export default {
   props: {
-
+    salutation: {
+      type: String,
+      default: ''
+    },
+    name: {
+      type: String,
+      default: ''
+    }
   },
   setup () {
-
+    return {
+      salutations
+    }
   }
 }
 </script>
