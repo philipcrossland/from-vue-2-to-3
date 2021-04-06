df8cff738d19370a27d5cf7df08f9dfbc4bbe039
Phil Crossland <phil@thirdwheelstudios.com>
2021-04-06 21:24:42 +0100

    End of L4 - v-model modifiers

--- src/components/SalutationName.vue
+++ src/components/SalutationName.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <select @change="$emit('update:salutation', $event.target.value)">
+    <select @change="updateSalutation">
       <option value=""></option>
       <option
         v-for="(item, index) in salutations"
@@ -13,7 +13,7 @@
     </select>
 
     <input
-    @input="$emit('update:name', $event.target.value)"
+    @input="updateName"
       :input="name"
       type="text"
     />
@@ -29,14 +29,44 @@ export default {
       type: String,
       default: ''
     },
+    salutationModifiers: {
+      default: () => ({})
+    },
     name: {
       type: String,
       default: ''
+    },
+    nameModifiers: {
+      default: () => ({})
     }
   },
-  setup () {
+  setup (props, { emit }) {
+    const updateSalutation = event => {
+      let val = event.target.value
+      if (props.salutationModifiers.capitalize) {
+        val = val.toUpperCase()
+      }
+
+      emit('update:salutation', val)
+    }
+
+    const updateName = event => {
+      let val = event.target.value
+      if (props.nameModifiers.capitalize) {
+        val = val.charAt(0).toUpperCase() + val.slice(1)
+      }
+
+      if (props.nameModifiers.reverse) {
+        val = val.split('').reverse().join('')
+      }
+
+      emit('update:name', val)
+    }
+
     return {
-      salutations
+      salutations,
+      updateSalutation,
+      updateName
     }
   }
 }
